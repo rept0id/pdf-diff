370505957755c7cf136781f09312e143063e3677
Rantouan Achmet <rad@simplecode.gr>
2024-06-03 10:38:49 +0300

    better structure and more streamlined packaging

--- package.sh
+++ package.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+### # Debian # ###
+
+# Packaging Dependencies
 sudo apt-get update
 sudo apt-get install dpkg-dev
-
+# Move executable to Debian's packaging wrapper
+cp ./src/pdfimagediff ./package/deb/src/usr/bin/pdfimagediff
+# Move to Debian's packaging wrapper and package
+cd ./package/deb
 dpkg-deb --build src
+# Rename the .deb with a more self-explanatory name
 mv src.deb pdf-image-diff.deb
+# Move back
+cd ../../..
